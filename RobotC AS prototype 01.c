813c25b829406151ab48a552efbe3df480be0676
tintinmovie <31559358+tintinmovie@users.noreply.github.com>
2017-09-05 17:55:04 +1200

    Added the prototype  01 robot c code
    First code that will be used for the current prototype 01 - for abominable snowflakes' robot

---
+++ RobotC AS prototype 01.c
@@ -0,0 +1,53 @@
+#pragma config(Motor,  port2,           LeftMotor,     tmotorVex269_HBridge, openLoop)
+#pragma config(Motor,  port3,           RightMotor,     tmotorVex269_HBridge, openLoop)
+#pragma config(Motor,  port4,           LiftMotor1,     tmotorVex269_HBridge, openLoop)
+#pragma config(Motor,  port5,           LiftMotor2,     tmotorVex269_HBridge, openLoop)
+#pragma config(Motor,  port6,           ClawMotor,     tmotorVex269_HBridge, openLoop)
+/* Code for Abomnible Snowflakes, In the Zone robot for Vex EDR - In the zone*/
+task main()
+{
+	while (true)
+	{
+		/* Drives left and right main motors */
+		motor[LeftMotor] = vexRT[Ch3];
+		motor[RightMotor] = vexRT[Ch2];
+
+		/*Lift up */
+		if(vexRT[Btn6U] == 1)
+		{
+			motor[LiftMotor1] = 50;
+			motor[LiftMotor2] = 50;
+		}
+		/*Lift down */
+		else if(vexRT[Btn6D] == 1)
+		{
+			motor[LiftMotor1] = -50;
+			motor[LiftMotor2] = -50;
+		}
+
+		/*Stop lift */
+		else
+		{
+			motor[LiftMotor1] = 0;
+			motor[LiftMotor2] = 0;
+		}
+		/*Open claw */
+		if(vexRT[Btn5U] == 1)
+		{
+			motor[ClawMotor] = 50;
+		}
+
+		/*Close claw */
+		else if(vexRT[Btn5D] == 1)
+		{
+			motor[ClawMotor] = -50;
+		}
+
+		/* Stop claw */
+		else
+		{
+			motor[ClawMotor] = 0;
+		}
+	}
+
+}
